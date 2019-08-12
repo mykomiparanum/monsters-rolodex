c9ea49a3ce89934e5a1afb7b21fedb534b1f0961
Myko <m.miparanum@gmail.com>
2019-08-12 15:17:16 +0800

    Updates

---
+++ precache-manifest.d2e814e48e12f94f037a3def8568d0ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccafe96432edfab129d5ffd38d7ff2b3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6f616f2fc63e3a33a672",
+    "url": "/monsters-rolodex/static/css/main.2277eb5a.chunk.css"
+  },
+  {
+    "revision": "90bc354edad42df017e0",
+    "url": "/monsters-rolodex/static/js/2.08bb4953.chunk.js"
+  },
+  {
+    "revision": "6f616f2fc63e3a33a672",
+    "url": "/monsters-rolodex/static/js/main.5c7b1a2e.chunk.js"
+  },
+  {
+    "revision": "7548f6fbafc46e9f04b0",
+    "url": "/monsters-rolodex/static/js/runtime~main.4eb04847.js"
+  }
+]);
